8c3e0d0c588e90688428e7c31f81e76e8a7fb0c0
marlonxteban <marlonxteban@gmail.com>
2021-03-16 23:33:13 -0500

    feat: Add cors configuration.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, abort
 from database.model import setup_db, Family, Succulent
+from flask_cors import CORS
 from config import config
 import json
 
@@ -9,6 +10,19 @@ def create_app():
     app.config.from_object(config)
     setup_db(app)
 
+    """
+    Cors config.
+    """
+
+    cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers',
+                             'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods',
+                             'GET,PUT,POST,PATCH,DELETE,OPTIONS')
+        return response
     """
     Families endpoints
     """
@@ -37,6 +51,33 @@ def create_app():
             "succulents": [succulent.format()
                            for succulent in succulents]
         })
+
+    '''
+    Error handlers.
+    '''
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "resource not found"
+        }), 404
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "bad request"
+        }), 400
 
     return app
 
